b5d6d1e722d971df60bd64fa1c82085c4dfa7d23
Ahmed Ihsan Erdem <ahmedihsanerdem@gmail.com>
2019-02-27 22:54:52 -0500

    project files created
    Signed-off-by: Ahmed Ihsan Erdem <ahmedihsanerdem@gmail.com>

---
+++ Tetris by ihsanerdem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tetris by ihsanerdem
+//
+//  Created by Erdem Meral on 2/27/19.
+//  Copyright © 2019 Ahmed Ihsan Erdem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tetris by ihsanerdem/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tetris by ihsanerdem
+//
+//  Created by Erdem Meral on 2/27/19.
+//  Copyright © 2019 Ahmed Ihsan Erdem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
